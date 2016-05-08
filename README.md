fe2369c3b55255bbb1d243ccc7024136daff5aea
Sreejith Unnikrishnan <sreejithu4u@gmail.com>
2016-05-08 12:47:47 -0700

    Added sensor list program

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Android projects
 
 1. Tip Calculator - A simple template for single activity applications. The app uses a button event to calculate tips based on the meal cost given by the user. 
+
+2. Sensor Monitor - This app list out all the sensors available on the current device.
---
+++ sensorMonitor/app/src/main/java/com/example/sreejith/sensormonitor/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.sreejith.sensormonitor;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    SensorManager mSensorManager;
+    TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+    }
+
+    public void findButtonClick(View v) {
+        List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        StringBuilder s = new StringBuilder();
+        for (int i=0; i<deviceSensors.size(); ++i) {
+            s.append(deviceSensors.get(i).getName()).append("\n");
+        }
+        resultTextView.setText(s.toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
